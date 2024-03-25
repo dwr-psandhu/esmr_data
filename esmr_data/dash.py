33308b06dcf06938627a0c8d3a072dabf8110df6
Nicky Sandhu <psandhu@water.ca.gov>
2024-03-25 13:09:18 -0700

    Refactor ESMRDash class to improve code readability and maintainability

--- esmr_data/dash.py
+++ esmr_data/dash.py
@@ -8,42 +8,73 @@ import numpy as np
 import pandas as pd
 import os
 import warnings
+
 warnings.filterwarnings("ignore")
 # viz libs
-hv.extension('bokeh')
+hv.extension("bokeh")
 pn.extension()
 
 
 class ESMRDash(pn.viewable.Viewer):
 
-    selected = param.List(
-        default=[0], doc='Selected node indices to display in plot')
+    selected = param.List(default=[0], doc="Selected node indices to display in plot")
 
     parameters = param.ListSelector(
-        default=['Flow', 'Temperature'],
-        objects=['Flow', 'Temperature', 'Dissolved Oxygen',
-                        'Electrical Conductivity @ 25 Deg. C'])
+        default=["Flow", "Temperature"],
+        objects=[
+            "Flow",
+            "Temperature",
+            "Dissolved Oxygen",
+            "Electrical Conductivity @ 25 Deg. C",
+        ],
+    )
 
-    location_place_type = param.ObjectSelector(default='Effluent Monitoring', objects=[
-                                               'Effluent Monitoring', 'Receiving Water Monitoring', 'Influent Monitoring', 'Internal Monitoring', 
-                                               'Groundwater Monitoring', 'Recycled Water Monitoring', 'Biosolids Monitoring', 'Stormwater Monitoring', 'Non-point Source Monitoring'])
+    location_place_type = param.ObjectSelector(
+        default="Effluent Monitoring",
+        objects=[
+            "Effluent Monitoring",
+            "Receiving Water Monitoring",
+            "Influent Monitoring",
+            "Internal Monitoring",
+            "Groundwater Monitoring",
+            "Recycled Water Monitoring",
+            "Biosolids Monitoring",
+            "Stormwater Monitoring",
+            "Non-point Source Monitoring",
+        ],
+    )
 
     def __init__(self, data, **params):
         super().__init__(**params)
         self.data = data  # type esmr.ESMR
         # map of facilities (averaging the locations for now) #FIXME:
         facility_location_lat_lon = esmr.build_facility_location_lat_lon(data.df)
-        self.facility_lat_lon = facility_location_lat_lon.groupby(
-            'facility_name').mean().reset_index()
-        self.facility_lat_lon = self.facility_lat_lon[['facility_name', 'latitude', 'longitude']]
+        self.facility_lat_lon = (
+            facility_location_lat_lon.groupby(
+                ["facility_name", "location", "location_desc"]
+            )
+            .mean()
+            .reset_index()
+        )
+        self.facility_lat_lon = self.facility_lat_lon[
+            ["facility_name", "latitude", "longitude"]
+        ]
         self.map = self.facility_lat_lon.hvplot.points(
-            x='longitude', y='latitude',
-            geo=True, tiles='CartoLight',
-            tools=['tap'],
-            hover_cols=['facility_name'],
-            nonselection_color='gray', nonselection_alpha=0.5, size=20,
-            ).opts(frame_width=400, aspect=1,
-                active_tools=['wheel_zoom', 'pan', 'tap'], title='Facilities with Lat/Lon information')
+            x="longitude",
+            y="latitude",
+            geo=True,
+            tiles="CartoLight",
+            tools=["tap"],
+            hover_cols=["facility_name"],
+            nonselection_color="gray",
+            nonselection_alpha=0.5,
+            s=50,
+        ).opts(
+            frame_width=400,
+            aspect=1,
+            active_tools=["wheel_zoom", "pan", "tap"],
+            title="Facilities with Lat/Lon information",
+        )
         select_map_pts = hv.streams.Selection1D(source=self.map.Points.I, index=[0])
         select_map_pts.add_subscriber(self.set_selected)
 
@@ -53,16 +84,18 @@ class ESMRDash(pn.viewable.Viewer):
         else:
             self.selected = index
 
-    @param.depends('selected', 'parameters')
+    @param.depends("selected", "parameters")
     def plot_facility(self):
         index = self.selected
         # Use only the first index in the array
         first_index = index[0]
         print(index)
-        return self.plot_parameters(self.data,
+        return self.plot_parameters(
+            self.data,
             self.facility_lat_lon.iloc[first_index, :].facility_name,
             self.parameters,
-            self.location_place_type)
+            self.location_place_type,
+        )
 
     def plot_parameters(self, data, facility_name, parameters, location_place_type):
         f = data.get_facility(facility_name)
@@ -72,15 +105,20 @@ class ESMRDash(pn.viewable.Viewer):
             for p in parameters:
                 vars_for_param = l.get_parameter(p).get_variables()
                 if len(vars_for_param) > 0:
-                    plt_for_param = hv.Overlay([v.result.hvplot() for v in vars_for_param])
-                    plots.append(plt_for_param.opts(title=f'{l.name}'))
+                    plt_for_param = hv.Overlay(
+                        [v.result.hvplot() for v in vars_for_param]
+                    )
+                    plots.append(plt_for_param.opts(title=f"{l.name}"))
         # bug in layout not being updated by DynamicMap
-        #plots[0] = plots[0].opts(title=facility_name)
+        # plots[0] = plots[0].opts(title=facility_name)
         # .opts(title=facility_name) # bug in layout not being updated in DynamicMap
         return hv.Layout(plots).cols(1).opts(title=facility_name)
 
     def __panel__(self):
-        return pn.Row(pn.Column(self.map,
-                            pn.Row(self.param.parameters, self.param.location_place_type),
-                            ),
-                            self.plot_facility)
+        return pn.Row(
+            pn.Column(
+                self.map,
+                pn.Row(self.param.parameters, self.param.location_place_type),
+            ),
+            self.plot_facility,
+        )
--- esmr_data/esmr.py
+++ esmr_data/esmr.py
@@ -1,6 +1,7 @@
 """
 Electronic Self Monitoring Report
 """
+
 from dataclasses import dataclass, field
 from typing import Any
 import pandas as pd
@@ -11,10 +12,10 @@ import warnings
 def warn_single_unique(df, column):
     vals = df[column].unique().astype(str)
     if len(vals) == 0:
-        warnings.warn(f'{column} does not have any unique values!')
-        return 'nan' # FIXME: what else to do?
+        warnings.warn(f"{column} does not have any unique values!")
+        return "nan"  # FIXME: what else to do?
     elif len(vals) > 1:
-        warnings.warn(f'{column} does not have a single unique value!: {vals}')
+        warnings.warn(f"{column} does not have a single unique value!: {vals}")
     else:
         return vals[0]
 
@@ -34,26 +35,35 @@ class Variable:
     rl: pd.DataFrame = field(repr=False, default=None)
 
     def __post_init__(self):
-        self.df = self.parameter.df.astype(
-            {'calculated_method': 'str'}).replace(
-            {'calculated_method': 'nan'}, '').query(
-            f'(units == "{self.units}") '+
-            f'& (calculated_method == "{self.calculated_method}")')
-        self.df = self.df.set_index('sampling_datetime').sort_index()
+        self.df = (
+            self.parameter.df.astype({"calculated_method": "str"})
+            .replace({"calculated_method": "nan"}, "")
+            .query(
+                f'(units == "{self.units}") '
+                + f'& (calculated_method == "{self.calculated_method}")'
+            )
+        )
+        self.df = self.df.set_index("sampling_datetime").sort_index()
         self.result = self.df.result
-        self.qualifier = "=" # FIXME:I don't understand DQ and DNQ with values in result column!
+        self.qualifier = (
+            "="  # FIXME:I don't understand DQ and DNQ with values in result column!
+        )
         self.analytical_method_code = warn_single_unique(
-            self.df, 'analytical_method_code')
-        #if self.qualifier == 'DQ' or self.qualifier == 'DNQ':
-        self.mdl = self.df[['mdl']]
-        self.ml = self.df[['ml']]
-        self.rl = self.df[['rl']]
-        #else:
+            self.df, "analytical_method_code"
+        )
+        # if self.qualifier == 'DQ' or self.qualifier == 'DNQ':
+        self.mdl = self.df[["mdl"]]
+        self.ml = self.df[["ml"]]
+        self.rl = self.df[["rl"]]
+        # else:
         qtext = self.qualifier if self.qualifier != "=" else ""
-        ctext = "["+self.calculated_method+"]" if self.calculated_method != "" else ""
+        ctext = (
+            "[" + self.calculated_method + "]" if self.calculated_method != "" else ""
+        )
         # FIXME: not sure how to handle multiple units specified. Selecting first one in the unique list.
-        self.result = self.df[['result']].rename(
-            {'result': f'{self.name} {ctext} ({self.units}) {qtext}'}, axis=1)
+        self.result = self.df[["result"]].rename(
+            {"result": f"{self.name} {ctext} ({self.units}) {qtext}"}, axis=1
+        )
 
 
 @dataclass
@@ -61,6 +71,7 @@ class Parameter:
     """
     Parameter is the value being measured. It can have one or more variables if measured in multiple units or calculated_method(s)
     """
+
     name: str
     location: Any
     df: pd.DataFrame = field(default=None, repr=False)
@@ -69,9 +80,15 @@ class Parameter:
         self.df = self.location.df.query(f'(parameter == "{self.name}")')
 
     def get_variables(self):
-        cols = ['calculated_method', 'units']
-        dfp = self.df[cols].astype(str).replace(
-            'nan', '').groupby(cols).count().reset_index()
+        cols = ["calculated_method", "units"]
+        dfp = (
+            self.df[cols]
+            .astype(str)
+            .replace("nan", "")
+            .groupby(cols)
+            .count()
+            .reset_index()
+        )
         return [Variable(self.name, *x, self) for x in dfp.values]
 
 
@@ -86,12 +103,12 @@ class Location:
 
     def __post_init__(self):
         self.df = self.facility.df.query(f'location == "{self.name}"')
-        self.place_type = warn_single_unique(self.df, 'location_place_type')
+        self.place_type = warn_single_unique(self.df, "location_place_type")
         # FIXME: location_place_id can be different from the same location name!!!
-        #self.place_id = assert_single_unique(self.df,'location_place_id')
-        #self.desc = assert_single_unique(self.df,'location_desc')
-        self.place_id = self.df['location_place_id'].unique().astype(str)
-        self.desc = self.df['location_desc'].unique().astype(str)
+        # self.place_id = assert_single_unique(self.df,'location_place_id')
+        # self.desc = assert_single_unique(self.df,'location_desc')
+        self.place_id = self.df["location_place_id"].unique().astype(str)
+        self.desc = self.df["location_desc"].unique().astype(str)
 
     def get_parameter_names(self):
         return self.df.parameter.unique().astype(str)
@@ -110,17 +127,19 @@ class Facility:
     def __post_init__(self):
         self.dfo = self.df
         self.df = self.df.query(f'facility_name == "{self.name}"')
-        self.region = self.df['region'].unique()[0]
-        self.place_id = self.df['facility_place_id'].unique()[0]
+        self.region = self.df["region"].unique()[0]
+        self.place_id = self.df["facility_place_id"].unique()[0]
 
     def get_location_names(self):
-        return self.df['location'].unique().astype(str)
+        return self.df["location"].unique().astype(str)
 
     def get_location(self, location):
         return Location(location, self)
 
-    def get_locations_of_type(self, location_place_type='Effluent Monitoring'):
-        locations = self.get_location_names() #TODO: could be more efficient with direct dataframe ops
+    def get_locations_of_type(self, location_place_type="Effluent Monitoring"):
+        locations = (
+            self.get_location_names()
+        )  # TODO: could be more efficient with direct dataframe ops
         locs = []
         for l in locations:
             loc = self.get_location(l)
@@ -134,72 +153,90 @@ class ESMR:
     df: pd.DataFrame
 
     def get_region_names(self):
-        return self.df['region'].unique().astype(str)
+        return self.df["region"].unique().astype(str)
 
     def get_facility_names(self, region=None):
         if region:
             dfx = self.df.query(f'region == "{region}"')
         else:
             dfx = self.df
-        return dfx['facility_name'].unique().astype(str)
+        return dfx["facility_name"].unique().astype(str)
 
     def get_facility(self, name):
         return Facility(name, self.df)
 
 
 def categorical_types():
-    categorical_types = {'region': 'category', 'location': 'category', 'location_place_type': 'category',
-                         'parameter': 'category', 'analytical_method_code': 'category', 'analytical_method': 'category',
-                         'calculated_method': 'category', 'qualifier': 'category', 'units': 'category',
-                         'review_priority_indicator': 'category', 'qa_codes': 'category',
-                         'receiving_water_body': 'category'}
+    categorical_types = {
+        "region": "category",
+        "location": "category",
+        "location_place_type": "category",
+        "parameter": "category",
+        "analytical_method_code": "category",
+        "analytical_method": "category",
+        "calculated_method": "category",
+        "qualifier": "category",
+        "units": "category",
+        "review_priority_indicator": "category",
+        "qa_codes": "category",
+        "receiving_water_body": "category",
+    }
     return categorical_types
 
 
 def read_data_csv(csv_file):
-    '''
+    """
     read data from csv and convert to appropriate data type. Also cache in .pkl (pickle) format for faster loads
-    '''
+    """
     fname, _ = os.path.splitext(csv_file)
-    pkl_file = fname+'.pkl'
+    pkl_file = fname + ".pkl"
     if os.path.exists(pkl_file):
         df = pd.read_pickle(pkl_file)
     else:
         df = pd.read_csv(csv_file, dtype=categorical_types())
-        df['sampling_datetime'] = pd.to_datetime(
-            df['sampling_date'] + ' ' + df['sampling_time'])
-        df = df.drop(['sampling_date', 'sampling_time'], axis=1)
-        df['analysis_datetime'] = pd.to_datetime(
-            df['analysis_date'] + ' ' + df['analysis_time'], errors='coerce')
-        df = df.drop(['analysis_date', 'analysis_time'], axis=1)
+        # TEMP FIX:
+        # df.loc[13330034, "sampling_date"] = "2016-11-30"
+        df["sampling_datetime"] = pd.to_datetime(
+            df["sampling_date"] + " " + df["sampling_time"], errors="coerce"
+        )
+        df = df.drop(["sampling_date", "sampling_time"], axis=1)
+        df["analysis_datetime"] = pd.to_datetime(
+            df["analysis_date"] + " " + df["analysis_time"], errors="coerce"
+        )
+        df = df.drop(["analysis_date", "analysis_time"], axis=1)
         df.to_pickle(pkl_file)
     return df
 
 
 def build_facility_location_lat_lon(df):
-    cols = ['latitude', 'longitude',
-            'facility_name', 'location', 'location_desc']
-    location_lat_lon = df.astype({'location': str}).groupby(
-        cols).count().reset_index()
+    cols = ["latitude", "longitude", "facility_name", "location", "location_desc"]
+    location_lat_lon = df.astype({"location": str}).groupby(cols).count().reset_index()
     # fixup obvious errors
     # +ve longitudes not in California, reverse the sign
-    lon_0plus = location_lat_lon.eval('longitude > 0')
-    location_lat_lon.loc[lon_0plus, 'longitude'] = - \
-        location_lat_lon.loc[lon_0plus, 'longitude']
+    lon_0plus = location_lat_lon.eval("longitude > 0")
+    location_lat_lon.loc[lon_0plus, "longitude"] = -location_lat_lon.loc[
+        lon_0plus, "longitude"
+    ]
     # -ve latitudes not in California, reverse the sign
-    lat_0minus = location_lat_lon.eval('latitude < 0')
-    location_lat_lon.loc[lat_0minus, 'latitude'] = - \
-        location_lat_lon.loc[lat_0minus, 'latitude']
-    # specific fix for couple of locations based on location_desc field 
-    location_lat_lon.loc[location_lat_lon.eval(
-        'latitude < 1'), 'latitude'] = 38+0/60.+29.73/3600.  # correction from location_desc of Lincoln Center GWT System
-    location_lat_lon.loc[location_lat_lon.eval('longitude > -40'), 'longitude'] = -(
-        121+3/60.+10.6/3600.)  # another one-off with longitude from description field
+    lat_0minus = location_lat_lon.eval("latitude < 0")
+    location_lat_lon.loc[lat_0minus, "latitude"] = -location_lat_lon.loc[
+        lat_0minus, "latitude"
+    ]
+    # specific fix for couple of locations based on location_desc field
+    location_lat_lon.loc[location_lat_lon.eval("latitude < 1"), "latitude"] = (
+        38 + 0 / 60.0 + 29.73 / 3600.0
+    )  # correction from location_desc of Lincoln Center GWT System
+    location_lat_lon.loc[location_lat_lon.eval("longitude > -40"), "longitude"] = -(
+        121 + 3 / 60.0 + 10.6 / 3600.0
+    )  # another one-off with longitude from description field
     return location_lat_lon
 
+
 def get_facilities_with_no_latlon_info(df, facility_location_lat_lon):
-    cols = ['facility_name']
+    cols = ["facility_name"]
     df_facilities = df[cols].astype(str).groupby(cols).count().reset_index()
-    df_facilites_merged = df_facilities.merge(
-        facility_location_lat_lon, how='outer')
-    return df_facilites_merged[(df_facilites_merged['latitude'].isnull()) | (df_facilites_merged['longitude'].isnull())]
+    df_facilites_merged = df_facilities.merge(facility_location_lat_lon, how="outer")
+    return df_facilites_merged[
+        (df_facilites_merged["latitude"].isnull())
+        | (df_facilites_merged["longitude"].isnull())
+    ]
